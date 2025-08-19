e70bf91d159df7b837c5fc2992194746c0bc4564
PavanPK07 <kalyanreddypallala@gmail.com>
2025-08-19 16:33:59 +0530

    shell

---
+++ vowels.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "enter statement:"
+
+read Statement
+
+if [ $Statement =~ "AEIOUaeiou" ];
+then
+    echo "$Statement has vowels in it"
+else 
+    echo "no vowels in $Statement"]
+fi
