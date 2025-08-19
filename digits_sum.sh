2c470e7a9d6fdbaf98eb08cc7502791f5a278520
PavanPK07 <kalyanreddypallala@gmail.com>
2025-08-19 17:09:54 +0530

    shell

--- digits_sum.sh
+++ digits_sum.sh
@@ -5,11 +5,11 @@ read Num
 Num=$n
 sum=0
 
-while [$n -ge 0 ]
+while [ $n -ge 0 ]
 do
-    digit=(($n%10))
-    sum=(($sum+$digit))
-    n=$((n/10))
+    digit=(( $n%10 ))
+    sum=(( $sum+$digit ))
+    n=$(( n/10 ))
 done
 
 echo " sum of digits of $Num is $sum "
