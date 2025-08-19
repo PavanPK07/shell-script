86afeceab16c98978e5ba1fbbdac7cd07ff5b252
PavanPK07 <kalyanreddypallala@gmail.com>
2025-08-19 16:11:04 +0530

    shell

--- line_count.sh
+++ line_count.sh
@@ -5,7 +5,8 @@ file_path=/home/centos/shell-script/if.sh
 if [ -f $file_path ];
 then
     line_count=$(wc -l < "$file_path")
-    echo "$file_path has $line_count lines"
+    word_count=$(wc -w < "$file_path")
+    echo "$file_path has $line_count lines with $word_count words"
 else
     echo "$file doesn't exist or empty"
 fi
