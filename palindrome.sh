01c54e91ef5e62855e3fab8f649a03529cd3e88b
PavanPK07 <kalyanreddypallala@gmail.com>
2025-08-19 12:37:01 +0530

    shell

---
+++ palindrome.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+list = ( madam level radar github amazon )
+for item in ${list[@]}
+do 
+    reversed = echo ("$item | rev")
+    if $item = $reversed 
+    then 
+        echo "$item is palindrome"
+    else
+        echo "$item is not a palindrome"
+    fi
+done
