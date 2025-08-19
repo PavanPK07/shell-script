03b9dc261620706c6dffae5adf64d70e6baab964
PavanPK07 <kalyanreddypallala@gmail.com>
2025-08-19 16:08:35 +0530

    shell

--- find_replace.sh
+++ find_replace.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-
 file_path=/home/centos/shell-script/shell.sh
 find_str=Purpose
 replace_str=Aim
 
-if [ -f $file_path ];
-then 
-    sed -i "/s/$find_str/$replace_str/g" "$file_path"
-    echo "replaced $find_str with $replace_str in $file_path"
+if [ -f "$file_path" ]; then 
+    sed -i "s/$find_str/$replace_str/g" "$file_path"
+    echo "Replaced $find_str with $replace_str in $file_path"
 else
-    echo "file doesn't exist or file doesnt contain $find_str"
+    echo "ERROR: File $file_path does not exist"
 fi
