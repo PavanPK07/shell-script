08c8bf86a5a8705040cd739de4b9fafe2c98a832
PavanPK07 <kalyanreddypallala@gmail.com>
2025-08-19 15:30:01 +0530

    shell

---
+++ line_count.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+file_path=/home/centos/shell-script/if.sh
+
+if [ $file_path -f ]
+then
+    line_count=(wc-l < $file_path)
+    echo "$file has $line_count lines"
+else
+    echo "$file doesn't exist or empty"
+fi
