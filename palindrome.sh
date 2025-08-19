962898089f3ece8c58f62a329ed0891d354773d3
PavanPK07 <kalyanreddypallala@gmail.com>
2025-08-19 12:47:45 +0530

    shell

--- palindrome.sh
+++ palindrome.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 words=(madam level radar github amazon)
-for word in ${words[@]}
-do 
+
+for word in "${words[@]}"
+do
     reversed=$(echo "$word" | rev)
-    if [ " $word = $reversed " ]
-    then 
-        echo "$word is palindrome"
-    else
-        echo "$word is not a palindrome"
+
+    if [ "$word" = "$reversed" ]
+    then
+        echo "$word is a palindrome"
     fi
 done
