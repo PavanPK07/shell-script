30d51ee1c66e3250d42fe5ebe5086b10a78495fd
PavanPK07 <kalyanreddypallala@gmail.com>
2025-08-19 16:06:45 +0530

    shell

--- find_replace.sh
+++ find_replace.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 
-file_path=/home/centos/shell-script/readme.MD
+file_path=/home/centos/shell-script/shell.sh
 find_str=Purpose
 replace_str=Aim
 
 if [ -f $file_path ];
 then 
-    sed -i "/s/$findstr/$replace_str/g" "$file_path"
+    sed -i "/s/$find_str/$replace_str/g" "$file_path"
     echo "replaced $find_str with $replace_str in $file_path"
 else
     echo "file doesn't exist or file doesnt contain $find_str"
